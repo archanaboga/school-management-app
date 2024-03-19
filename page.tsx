6f1f5da6e4b02c9b578affdad77fd2e648de4616
archanaboga <140945242+archanaboga@users.noreply.github.com>
2024-03-19 19:40:56 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className="flex items-center justify-center h-screen w-full text-center">
+    <p>HELLO WORLd</p>
+    <p>school</p>
+  </section>
+  
+  );
+}
